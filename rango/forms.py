6857abc2850468cd0247b43862ea57395cc2c5f6
Fergus Young <2671493y@student.gla.ac.uk>
2023-02-09 13:58:41 +0000

    commit after chapter 7

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,32 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, name_max_length, url_max_length
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
-    views = forms.IntegerField(widget.forms.HiddenInput(), initial=0)
+    name = forms.CharField(max_length=name_max_length, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Category
+        fields = ('name',)
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=name_max_length, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=url_max_length, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
+"""
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        #If url is not empty and doesnt start with "http://", then prepend the "http://"
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
+"""
