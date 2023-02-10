38e90ff4d383954483032b5c9e9f9f6477ab1c79
Fergus Young <2671493y@student.gla.ac.uk>
2023-02-10 16:47:01 +0000

    part way chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -19,6 +19,29 @@ class PageForm(forms.ModelForm):
     class Meta:
         model = Page
         exclude = ('category',)
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
+
+
+
+
+
+
+
+
+
+
+
 """
     def clean(self):
         cleaned_data = self.cleaned_data
